db2af8db383a154e69530e2afff9707b73b4b29e
Jenn Eng <Jenn@Jennifers-MacBook-Pro-2.local>
2016-08-23 12:59:07 -0400

    Added tests.

--- lib/duckboat.rb
+++ lib/duckboat.rb
@@ -1,21 +1,23 @@
-# require_relative 'boat'
-# require_relative 'water_fowl'
-# require_relative 'passenger'
-#
-# class Duckboat
-#   include Water_fowl
-#   include Boat
-#
-#   attr_reader :passengers
-#
-#   def initialize
-#     @passengers = []
-#   end
-#
-#   def load_passenger(name)
-#     if @passengers.size < passenger_capacity
-#       @passengers << Passenger.new(name)
-#       puts "Welcome aboard, #{name}"
-#     end
-#   end
-# end
+require_relative 'boat'
+require_relative 'water_fowl'
+require_relative 'passenger'
+
+class Duckboat
+  include Boat
+  include Water_fowl
+
+  attr_reader :passengers
+  attr_accessor :captain
+
+  def initialize
+    @passengers = []
+    @captain = nil
+  end
+
+  def load_passenger(name)
+    if @passengers.size < passenger_capacity
+      @passengers << Passenger.new(name)
+      puts "Welcome aboard, #{name}"
+    end
+  end
+end
---
+++ spec/duckboat_spec.rb
@@ -0,0 +1,26 @@
+require_relative "../lib/duckboat"
+require 'pry'
+
+RSpec.describe Duckboat do
+  let(:new_boat) { Duckboat.new }
+
+  describe "#terrestrial?" do
+    it "returns true" do
+      expect(new_boat.terrestrial?).to eq true
+    end
+  end
+
+  describe "#has_engine?" do
+    it "returns true" do
+      expect(new_boat.has_engine?).to eq true
+    end
+  end
+
+  describe "#captain" do
+    it "returns the name of the captain" do
+      new_boat.captain = Passenger.new("Nick")
+      expect(new_boat.captain.name).to eq "Nick"
+    end
+  end
+
+end
